90567a87df7b93ab790c11cc959386576e556141
abdul mukhsin <aabdulmukhsin@yahoo.com>
2025-09-19 14:18:34 +0000

    fix:logo update

--- src/components/core/Sidebar.tsx
+++ src/components/core/Sidebar.tsx
@@ -1,6 +1,5 @@
 'use client'
-import {FaPills} from "react-icons/fa6";
-import {FaChartLine, FaFileMedicalAlt} from "react-icons/fa";
+import {FaFileMedicalAlt} from "react-icons/fa";
 import {MdDashboard} from "react-icons/md";
 import {useState} from "react";
 import Link from "next/link";
@@ -8,8 +7,8 @@ import Link from "next/link";
 
 const Sidebar = () => {
     const tabs = [
-        {title: "Dashboard", icon: <MdDashboard/>, path:"/dashboard"},
-        {title: "Transactions", icon: <FaFileMedicalAlt/>,path:"/transactions"},
+        {title: "Dashboard", icon: <MdDashboard/>, path: "/dashboard"},
+        {title: "Transactions", icon: <FaFileMedicalAlt/>, path: "/transactions"},
         // {title: "Monitor patient", icon: <FaChartLine/>,path:"monitor-patient"},
 
     ]
@@ -17,21 +16,14 @@ const Sidebar = () => {
 
     return <aside className="w-full bg-gray-50 border-r border-gray-200 min-h-screen">
         <div className="p-6">
-            <div className="flex items-center space-x-2 mb-8">
-                <div className="w-8 h-8 rounded-lg flex items-center justify-center">
-                    <img src={"/logo.png"} alt="logo" />
-                </div>
-                <div>
-                    <h2 className="font-semibold text-red-600">Royal Estate</h2>
-                    {/*<p className="text-xs text-gray-500">Healthcare Portal</p>*/}
-                </div>
+            <div className="flex items-center justify-center mb-8">
+                <img src={"/logo.png"} className="w-[120px]" alt="logo"/>
             </div>
-
             <nav className="space-y-2">
                 {tabs.map(tab => <Link href={tab.path} onClick={() => setActiveTab(tab.path)} key={tab.path}
-                    className={`w-full flex items-center space-x-3 px-3 py-2 rounded-lg text-left ${
-                        activeTab === tab.path ? 'bg-blue-100 text-blue-700' : 'text-gray-600 hover:bg-gray-100'
-                    }`}
+                                       className={`w-full flex items-center space-x-3 px-3 py-2 rounded-lg text-left ${
+                                           activeTab === tab.path ? 'bg-blue-100 text-blue-700' : 'text-gray-600 hover:bg-gray-100'
+                                       }`}
                 >
                     {tab.icon}
                     <span>{tab.title}</span>
